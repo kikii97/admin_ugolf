95ed711cd3ee2fa0fd651009aec113071e56b283
kikii97 <kikii23kurnia@gmail.com>
2024-11-06 13:23:20 +0700

    Push pertama Admin UGOLF

---
+++ resources/views/ugolf.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UGOLF</title>
+    <!-- Google Fonts Kufam -->
+    <link href="https://fonts.googleapis.com/css2?family=Kufam:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}" />
+
+</head>
+
+<body>
+    <div class="container">
+        <h1>UGOLF</h1>
+        <a href="#" class="btn">
+            <img src="assets/images/Ticket.png" alt="Ticket Icon" width="25">
+            BELI TIKET
+        </a>
+    </div>
+</body>
+
+</html>
